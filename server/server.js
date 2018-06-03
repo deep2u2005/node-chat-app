5dd2759805d00b801c6d41f58885e95a3cf35fca
deep2u2005 <deep2u2005@gmail.com>
2018-06-03 18:31:33 +0530

    Init commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '/../public');
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Started listening on port: ', port);
+});
+
