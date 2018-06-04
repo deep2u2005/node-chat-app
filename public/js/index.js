96734fd970e23d9e7ef308232da3b5b050046feb
deep2u2005 <deep2u2005@gmail.com>
2018-06-04 13:28:30 +0530

    Message Users

--- public/js/index.js
+++ public/js/index.js
@@ -9,12 +9,23 @@ socket.on('disconnect', function () {
     console.log('disconnected from the server');
 });
 
-socket.on('newMsg', function (msg) {
-    console.log('newMsg: ', msg);
+socket.on('newMsg', function (message) {
+    console.log('newMsg: ', message);
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.msg}`);
+    $('#messages').append(li);
 });
 
 socket.on('chtMsg', function (msg) {
     console.log('chtMsg: ', msg);
 });
 
+$('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMsg', {
+        from: 'User',
+        msg: $('[name=message]').val()
+    }, function () {
 
+    });
+});
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const { generateMessage } = require('./utils/message');
 const publicPath = path.join(__dirname, '/../public');
 
 var port = process.env.PORT || 3000;
@@ -24,9 +24,10 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMsg', generateMessage('Admin', 'New user joined'));
 
-    socket.on('createMsg', (message) => {
+    socket.on('createMsg', (message, callback) => {
         console.log('createMsg', message);
         io.emit('newMsg', generateMessage(message.from, message.msg));
+        callback('This is from the server!');
         // socket.broadcast.emit('newMsg', {
         //     from: message.from,
         //     msg: message.msg
